f22d24761a8d42867c742ddbe61b2af82e63b872
blackmax <blackmax1886@gmail.com>
2024-06-26 16:54:11 +0900

    Feat(auth): on signIn, create user in db

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,12 +1,9 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import {
-  getServerSession,
-  type DefaultSession,
-  type NextAuthOptions,
-} from "next-auth";
+import type { DefaultSession, NextAuthOptions } from "next-auth";
+import { getServerSession } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
 import { env } from "~/env";
+import { db } from "~/server/db";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -35,15 +32,6 @@ declare module "next-auth" {
  * @see https://next-auth.js.org/configuration/options
  */
 export const authOptions: NextAuthOptions = {
-  callbacks: {
-    session: ({ session, user }) => ({
-      ...session,
-      user: {
-        ...session.user,
-        id: user.id,
-      },
-    }),
-  },
   providers: [
     DiscordProvider({
       clientId: env.DISCORD_CLIENT_ID,
@@ -59,6 +47,31 @@ export const authOptions: NextAuthOptions = {
      * @see https://next-auth.js.org/providers/github
      */
   ],
+  callbacks: {
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id,
+      },
+    }),
+    signIn: async ({ user, account }) => {
+      if (!account) throw new Error("No account found");
+      const existingUser = await db.user.findUnique({
+        where: { accountId: account?.providerAccountId },
+      });
+      if (!existingUser) {
+        await db.user.create({
+          data: {
+            name: user.name,
+            email: user.email,
+            accountId: account?.providerAccountId,
+          },
+        });
+      }
+      return true;
+    },
+  },
 };
 
 /**
